5365670ab27795ddcfbcd6266ef2c41e7160ad99
Angelo Longoria <arlongoria93@gmail.com>
2022-03-10 12:13:39 -0600

    Chakra Installed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import type { AppProps } from "next/app";
 
 import React from "react";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <ChakraProvider>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </ChakraProvider>
   );
 }
