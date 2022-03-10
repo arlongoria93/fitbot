48d197c07a9c9823de8427661587c2caa7ed6453
Angelo Longoria <arlongoria93@gmail.com>
2022-03-10 11:44:45 -0600

    installed/configured auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import React from "react";
+import { UserProvider } from "@auth0/nextjs-auth0";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
-
-export default MyApp
---
+++ pages/user/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import React from "react";
+import Profile from "../../components/Profile";
+const User: NextPage = () => {
+  return (
+    <>
+      <Profile />
+    </>
+  );
+};
+
+export default User;
