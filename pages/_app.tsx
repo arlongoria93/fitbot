dcc5e133bcec11cb1842aaf6c639ccd9448bb70b
Angelo Longoria <arlongoria93@gmail.com>
2022-03-12 20:19:04 -0600

    Landing page created
    finished with landing page. should show users name if logged and not if not logged in.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { theme } from "../theme/theme";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function App({
   Component,
@@ -12,6 +13,9 @@ export default function App({
   return (
     <ChakraProvider theme={theme}>
       <SessionProvider session={session}>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.svg" />
+        </Head>
         <Component {...pageProps} />
       </SessionProvider>
     </ChakraProvider>
